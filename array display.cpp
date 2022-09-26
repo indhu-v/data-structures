a31a9c5f3b6decffd998c14d98dcc03b6803f3ce
indhu-v <113018637+indhu-v@users.noreply.github.com>
2022-09-26 23:34:58 +0530

    Add files via upload

---
+++ array display.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int i, n, arr[20];
+printf("\n Enter the number of elements to store in the array : ");
+scanf("%d", & n);
+for(i=0;i<n;i++)
+{
+printf("\n arr[%d] = ", i);
+scanf("%d",& arr[i]);
+}
+printf("\n The array elements are ");
+for(i=0;i<n;i++)
+printf("\t %d", arr[i]);
+return 0;
+}
